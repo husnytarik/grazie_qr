5efe3e7c3de3c1d0a8a046d85e3ea5b879144ab7
husnytarik <husnytarik@outlook.com>
2026-04-06 16:50:31 +0300

    Config Created

--- menu.js
+++ menu.js
@@ -2,16 +2,6 @@
    Coffee Grazie — Menu (Firebase)
 ═══════════════════════════════════════ */
 
-// ── FIREBASE CONFIG ──
-const firebaseConfig = {
-  apiKey: "AIzaSyChZQs0aute9V5tnFvnSQa6laHhrCjDfmk",
-  authDomain: "grazie-2a2a7.firebaseapp.com",
-  projectId: "grazie-2a2a7",
-  storageBucket: "grazie-2a2a7.firebasestorage.app",
-  messagingSenderId: "48962649572",
-  appId: "1:48962649572:web:0926d8484ad0640d491bf1",
-};
-
 if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
